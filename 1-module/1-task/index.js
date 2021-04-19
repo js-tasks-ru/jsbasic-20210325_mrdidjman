c4115d114cd66c908be7dbe6b755c3e080f1194c
Dmitry Kozlov <mrdidjman@gmail.com>
2021-04-19 20:34:20 +0300

    решил задачи первого блока

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,13 @@
 function factorial(n) {
-  // ваш код...
+  let i=0;
+  let fact=1;
+  while (i<(n+1)) {
+    if ((i === 0 ) || (i === 1)) {
+      i++;
+    } else {
+      fact=fact*i;
+      i++;
+    }
+  }
+  return fact;
 }
